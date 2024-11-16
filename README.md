b4b80e4bc8f752b7519701ba5b973d40f92adb46
NabinPaudel001 <paudelnabin41@gmail.com>
2024-11-16 19:40:03 +0545

    setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# hultprize_2024
+## Getting Started
+Create Components For Every update you make.
+
+## Deployment 
+The project will be added to deployment only after you commit is verified.
---
+++ website/src/app/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="justify-center">
+      <h1>Hello Mic testing Mic testing 1...2..3.</h1>
+    </div>
+  );
+}
